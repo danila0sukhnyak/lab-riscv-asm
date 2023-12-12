f258616c218702589d2b1f41b05da6dced1fa86b
danila0sukhnyak <70540840+danila0sukhnyak@users.noreply.github.com>
2023-12-12 21:48:28 +0300

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-#define N 3
-#define M 4
+#define N 4 // Number of columns
+#define M 2 // Number of rows
 
-void process(int n, int m, int matrix [M][N], int *result)
+void process(int n, int m, int matrix[M][N], int *result)
 {
     for (int i = 0; i < m; ++i) {
         result[i] = 0;
         for (int j = 0; j < n; ++j) {
-            result[i] += matrix[i][j];
+            if (matrix[i][j] > 0) {
+                result[i] += matrix[i][j]; // Sum only positive elements
+            }
         }
     }
-
 }
 
 int main(int argc, char** argv)
@@ -19,6 +20,7 @@ int main(int argc, char** argv)
     int matrix[M][N];
     int result[M];
     
+    // Reading matrix values
     for (int i = 0; i < M; ++i) {
         for (int j = 0; j < N; ++j) {
             scanf("%d", &matrix[i][j]);
@@ -27,6 +29,7 @@ int main(int argc, char** argv)
 
     process(N, M, matrix, result);
 
+    // Printing the sum of positive elements for each row
     for (int i = 0; i < M; ++i) {
         printf("%d ", result[i]);
     }
