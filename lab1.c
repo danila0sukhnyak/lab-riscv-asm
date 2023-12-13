723a564bce2b08d0b5914a2023c0b70985b9a4ef
danila0sukhnyak <70540840+danila0sukhnyak@users.noreply.github.com>
2023-12-13 17:52:34 +0300

    fix lab1.c

--- lab1.c
+++ lab1.c
@@ -8,7 +8,9 @@ void process(int n, int m, int matrix[M][N], int *result)
     *result = 0;  
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
-            *result += matrix[i][j];  
+            if (matrix[i][j] > 0) {
+                *result += matrix[i][j]; 
+            }
         }
     }
 }
